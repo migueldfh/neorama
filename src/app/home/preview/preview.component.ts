f09c749279178e389f776ce5ea4285dae9b3eb02
Mariana Zamarripa <marianahndez@Marianas-MacBook-Air.local>
2020-07-29 18:43:36 -0500

    Responsive styles fixes v.4

--- src/app/home/preview/preview.component.ts
+++ src/app/home/preview/preview.component.ts
@@ -13,13 +13,17 @@ export interface DialogData {
 export class PreviewComponent implements OnInit {
   img: any;
   name: any;
+  description: any;
   price: any;
   address: any;
   bathrooms: any;
   built_size: any;
+  total_built: any;
   parking: any;
   contact: boolean;
   currency_type: any;
+  consultor_name: any;
+  consultor_phone: any;
   message: string;
 
   constructor(
@@ -28,12 +32,16 @@ export class PreviewComponent implements OnInit {
   ) { 
     //Obtener datos de la propiedad
     this.img = data.propObj['images'];
-    this.name = data.propObj['ad_description'];
+    this.name = data.propObj['name'];
+    this.description = data.propObj['ad_description'];
     this.price = data.propObj['price'];
     this.built_size = data.propObj['built_size'];
+    this.total_built = data.propObj['total_built'];
     this.bathrooms = data.propObj['bathrooms'];
     this.parking = data.propObj['parking'];
     this.address = data.propObj['delegation'];
+    this.consultor_name = data.propObj['consultor'];
+    this.consultor_phone = 81248940;
     this.currency_type = data.propObj['currency_type'];
     this.contact = false;
     this.message = "Hola, me interesa este inmueble que vi en su página web y quiero que me contacten. Gracias.";
